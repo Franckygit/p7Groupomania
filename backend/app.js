21781590d710f9b41b58c7fa2b791822fd212250
Franckygit <franck.gauthier016@gmail.com>
2022-08-11 19:42:17 +0200

    file creation

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+// Importe les modules installés au préalable
+const express = require("express");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const auth = require("./middleware/auth");
+const userRoutes = require("./routes/users")
+const sauceRoutes = require("./routes/sauces")
+const path = require("path")
+require('dotenv').config()
+
+
+const app = express();
+app.use(express.json());
+// ------------------------------------------------------------------------
+
+// Pour se connecter à la BDD
+mongoose
+  .connect(process.env.MONGOOSE_CONNECT)
+  .then(() => console.log("Connecté à MongoDB"))
+  .catch(() => console.log("Échec de la connexion à MongoDB"));
+// ------------------------------------------------------------------------
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+// ------------------------------------------------------------------------
+
+// Routes d'API
+app.use("/api/auth",userRoutes);
+//app.use("/api/sauces",sauceRoutes);
+app.use("/images",express.static(path.join(__dirname,"images"))); //chemin statique images
+// -------------------------------------------------------------------------
+
+module.exports = app;
---
+++ backend/middleware/password-strength.js
@@ -0,0 +1,11 @@
+const { passwordStrength } = require('check-password-strength')
+
+
+module.exports = (req, res, next) => {
+  const password_strength = passwordStrength(req.body.password)
+
+  if (password_strength.id < 2){
+    res.status(411).json({message: "Votre mot de passe doit contenir: minuscules/majuscules, chiffres, symboles sur 8 caractères minimum. "})
+  } else{
+    next()
+  }}
